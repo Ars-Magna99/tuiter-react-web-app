7bd088ca5a4b5dcbb26c90906d50dc5b17ecf466
Ziyang Wang <ziyangwang@Ziyangs-MBP-2021.local>
2022-11-16 18:11:48 -0500

    a8 REST fa22

--- src/tuiter/home/whats-happening.js
+++ src/tuiter/home/whats-happening.js
@@ -12,7 +12,7 @@ const WhatsHappening = () => {
       const newTuit = {
         tuit: whatsHappening,
         avatarIcon:"../images/NASA_logo.png",
-        dislike:0,
+        dislikes:0,
         handle:"@wzy0909",
         time: "just posted"
       }
